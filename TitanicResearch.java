e7d7b01210d99f92ed62472fc75b42a7e5f5e839
lsorensen3 <90704969+lsorensen3@users.noreply.github.com>
2021-11-02 21:24:28 -0400

    comments with set up for each method

---
+++ TitanicResearch.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+public class TitanicResearch(){
+
+  public static void main(String[] args){
+
+  }
+
+  public static String[][] allData(File pathname){
+    //method that takes all information from the titanic3.csv and puts data into 2D ArrayList
+    //embedded while loop that will iterate through each token and add them to a String[][]
+    //the String[] in the String[][] will be one for each person's data (one line in csv)
+
+
+    }
+
+
+  }
+
+  public static double ticketPrice(String name){
+    //iterate through each data line to find price of ticket using name
+    //use a while loop (while next is true) to iterate through names
+    //once find an exact name, find and return the price of the ticket
+    //price is
+
+  }
+
+  public static boolean survival(String name){
+    //iterate through each data line to find if the person survived
+    //while loop to find name of passenger, return true or false for survival
+
+
+  }
+
+  public static String findGender(String name){
+    //iterate through each data line to find gender of person
+    //while loop to find name, return F for female, M for male
+
+  }
+
+  public static int findAge(String name){
+    //iterate through each data line to find the age of each person
+    //find name, return age
+
+  }
+
+  public static double mostExpensiveTicketSurvival(){
+    //method that finds the people with the most expensive tickets and sees if they survived
+    //split up by top half of expensive and bottom half and compare? or maybe top, middle, bottom and compare
+
+
+    //return percentage of survival
+  }
+
+  public static double genderSurvival(){
+    //method that determines whether there is a trend between gender and genderSurvival
+    //while loop that iterates through each person, adds 1 to female if F survived and opp for men
+    //when iterating through genders, add one to female one to male (total f passengers and m)
+    //compares the female and male survivals (compare female/male survival percentages)
+  }
+
+
+}
